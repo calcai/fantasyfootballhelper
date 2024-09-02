490677483092b946f37dd4e3e2bd67f6e1d052c0
Calvin Cai <calvincai@Calvins-MacBook-Pro.local>
2024-09-02 11:35:37 -0400

    populated database, initialized spring boot backend

---
+++ backend/fantasyfootballdraftguide/src/test/java/com/calvin/fantasyfootballdraftguide/FantasyfootballdraftguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calvin.fantasyfootballdraftguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyfootballdraftguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
